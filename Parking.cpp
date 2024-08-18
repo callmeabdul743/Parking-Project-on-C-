10054dc99338cb32cdb200424515029fea214da9
callmeabdul743 <abdullahkhalidmul@gmail.com>
2024-08-18 13:20:21 +0500

    Add all the file and debug all the problem

--- Parking.cpp
+++ Parking.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
 using namespace std;
 int main(){
-while(ture){
-    start:
-    int i;
     int capacity=100;
-    int amount=0;
     int count=0;
-    int bike=0, car=0, bus=0;
+    int i, amount=0, bike=0, car=0, bus=0;
+while(true){
     //Menu
+    cout<<"\n***///////////////////////////////////////////***\n";
     cout<<"Press 1 for the entry of the bike Rs.100"<<endl;
     cout<<"Press 2 for the entry of the car Rs.200"<<endl;
     cout<<"Press 3 for the entry of the bus Rs.300"<<endl;
     cout<<"Press 4 see the all the entries"<<endl;
     cout<<"Press 5 to delete all the entries"<<endl;
     cin>>i;
+    cout<<"/////////////////////////////////////////////////\n";
 
     //for bike(1)
     if(count <= capacity){
         if(i==1){
         amount= amount+100;
-        count++;
+        count = count+1;
         bike++;
         }
 
     //for car(2)
         else if(i==2){
         amount= amount+200;
-        count++;
+        count = count+1;
         car++;
         }
 
     //for car(3)
         else if(i==3){
         amount= amount+300;
-        count++;
+        count = count+1;
         bus++;
         }
     
@@ -64,7 +63,6 @@ while(ture){
     //else condition
         else{
             cout<<"Invalid Input";
-            goto start;
         }
     }
     else{
