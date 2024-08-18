e20c289aa63a3101b004719346afd8134a7eebda
callmeabdul743 <abdullahkhalidmul@gmail.com>
2024-08-18 12:32:15 +0500

    add the menu

--- Parking.cpp
+++ Parking.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Press 1 for the entry of "
+    cout<<"Press 1 for the entry of the car Rs.100"<<endl;
+    cout<<"Press 2 for the entry of the car Rs.200"<<endl;
+    cout<<"Press 3 for the entry of the bus Rs.300"<<endl;
+    cout<<"Press 4 see the all the entries"<<endl;
+    cout<<"Press 5 to delete all the entries"<<endl;
+
+
     return 0;
 }
